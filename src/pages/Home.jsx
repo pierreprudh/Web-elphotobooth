bcc88c92e05990fd1f9b474786c2c9da3ac6c61d
pierreprudh <prudh.pierre@gmail.com>
2025-07-16 19:16:31 +0200

    Relocating files

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,12 @@
 import { useEffect, useState } from "react";
 import { Navbar } from "../components/Navbar";
-import { AboutSection } from "./AboutSection";
-import { ServiceSection } from "./ServiceSection";
-import { HeroSection } from "./HeroSection";
-import { Footer } from "./Footer";
-import { ContactSection } from "./ContactSection";
-import { PartnerSection } from "./PartnerSection";
-import { AuroraBackground } from "./AuroraBackground";
+import { AboutSection } from "../components/AboutSection";
+import { ServiceSection } from "../components/ServiceSection";
+import { HeroSection } from "../components/HeroSection";
+import { Footer } from "../components/Footer";
+import { ContactSection } from "../components/ContactSection";
+import { PartnerSection } from "../components/PartnerSection";
+import { AuroraBackground } from "../components/AuroraBackground";
 
 
 export const Home = () => {
